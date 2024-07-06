ac57ec66637195642ccf1bdfbc50683b32f33109
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-07-06 22:22:28 +0200

    Project Lotus >> V2.0.7
    V2.0.7 here. Analytics

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react"
+import Head from "next/head";
 
 export default function RootLayout({
     children,
@@ -7,7 +9,17 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className="antialiased">{children}</body>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="description" content="Discover Project Lotus, a relaxing platform designed for studying with the Pomodoro Method. Enhance your focus and productivity with soothing backgrounds and a built-in 25-minute timer. Perfect for students and professionals looking to optimize their study sessions. Visit us now!" />
+                <title>Project Lotus</title>
+                <link rel="icon" type="image/png" href="../../../public/icon.ico"></link>
+            </Head>
+            <body className="antialiased">
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
