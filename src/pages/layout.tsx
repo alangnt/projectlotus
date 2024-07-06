d99e84235cf1b0096f45ce0957ec13082e251636
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-07-06 22:36:07 +0200

    Project Lotus >> V2.0.9
    V2.0.9 here.

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
     children,
@@ -11,6 +12,7 @@ export default function RootLayout({
             <body className="antialiased">
                 {children}
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
