a4c14757a544df5adde072c7fc05618684abd266
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-07-07 08:53:17 +0200

    Project Lotus >> V2.1.1
    V2.1.1 here. Corrected responsive design

--- src/components/footer/footerHome.tsx
+++ src/components/footer/footerHome.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="flex w-full justify-center items-center h-1/10">
-            <p>&copy; 2024 Project Lotus | Alan GEIRNAERT. All rights reserved.</p>
+            <p className="footer-size">&copy; 2024 Project Lotus | Alan GEIRNAERT. All rights reserved.</p>
         </footer>
     )
 }
