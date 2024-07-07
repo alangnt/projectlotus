3ffda3b6ffc4aa447ca2d8400286b3fe4f1dbf85
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-07-07 07:42:23 +0200

    Project Lotus >> V2.1
    V2.1 here. Updated music component so that it can be a live streaming on youtube instead of a spotify playlist

--- src/components/main/comps/music.tsx
+++ src/components/main/comps/music.tsx
@@ -1,9 +1,10 @@
 export default function MusicPlaylist() {
 
     return (
-        <div className="music-div">
-            <iframe title="Spotify Playlist" src="https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM?utm_source=generator&theme=0"
-                frameBorder="0" width="30%" height="152" allowFullScreen allow="encrypted-media"></iframe>
+        <div className="music-div pl-4">
+            <iframe width="392" height="220.5" src="https://www.youtube.com/embed/jfKfPfyJRdk?si=6lmhNmcR9IMxMr5d" title="YouTube video player"
+                frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     )
 }
