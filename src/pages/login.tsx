8d6b026882418a1bee91e6e737e2bc4bb3ca42c6
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-07-21 21:06:14 +0200

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,6 +12,10 @@ const Connect: React.FC = () => {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="google-site-verification" content="JNRzwjtOEUwxtu9LWSm1sdHseLr5Jg-FMLKA6LZbphI" />
                 <meta name="description" content="Discover Project Lotus, a relaxing platform designed for studying with the Pomodoro Method. Enhance your focus and productivity with soothing backgrounds and a built-in 25-minute timer. Perfect for students and professionals looking to optimize their study sessions. Visit us now!" />
+
+                <meta data-rh='true' property='og:type' content='website' />
+                <meta data-rh='true' property='og:image' content='/img/projectlotus.png' />
+
                 <title>Project Lotus | Login to your Account</title>
                 <link rel="icon" href="/icon.ico"></link>
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8441329308595760"
