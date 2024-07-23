5a886d8110347fda05bba23ae4593fb429167bed
Alan Geirnaert <geirnaert.alan@icloud.com>
2024-07-23 20:38:13 +0200

    update

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -9,6 +11,7 @@ export default function RootLayout({
         <html lang="en">
             <body className="antialiased">
                 {children}
+                <Analytics />
             </body>
         </html>
     );
